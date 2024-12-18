a7d5cf8bd6d745331c1056f7837c9a4e35d8a5d3
Paritosh Pal <paritoshpal7796@gmail.com>
2024-12-18 10:46:32 +0530

    Mapper now workking

--- dhruvzon/src/main/java/com/Ecommerce/dhruvzon/mapper/UserMapper.java
+++ dhruvzon/src/main/java/com/Ecommerce/dhruvzon/mapper/UserMapper.java
@@ -2,17 +2,50 @@ package com.Ecommerce.dhruvzon.mapper;
 
 import com.Ecommerce.dhruvzon.dto.user.UserRequestDTO;
 import com.Ecommerce.dhruvzon.dto.user.UserResponseDTO;
+import com.Ecommerce.dhruvzon.enums.Status;
 import com.Ecommerce.dhruvzon.model.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Service;
 
+@Service
+public class UserMapper {
+    // Map UserRequestDTO to User
+    public User userRequestDTOToUser(UserRequestDTO userRequestDTO) {
+        if (userRequestDTO == null) {
+            return null;
+        }
 
-@Mapper(componentModel = "spring")
-public interface UserMapper {
+        User user = new User();
+        user.setFirstName(userRequestDTO.getFirstName());
+        user.setLastName(userRequestDTO.getLastName());
+        user.setEmail(userRequestDTO.getEmail());
+        user.setPassword(userRequestDTO.getPassword());  // Password should be encoded before saving
+        user.setPhoneNumber(userRequestDTO.getPhoneNumber());
+        user.setAddress(userRequestDTO.getAddress());
+        user.setRole(userRequestDTO.getRole());
+        user.setStatus(Status.ACTIVE);  // Default status
 
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+        return user;
+    }
 
-    User userRequestDTOToUser(UserRequestDTO userRequestDTO);
+    // Map User to UserResponseDTO
+    public UserResponseDTO userToUserResponseDTO(User user) {
+        if (user == null) {
+            return null;
+        }
 
-    UserResponseDTO userToUserResponseDTO(User user);
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+        userResponseDTO.setId(user.getId());
+        userResponseDTO.setFirstName(user.getFirstName());
+        userResponseDTO.setLastName(user.getLastName());
+        userResponseDTO.setEmail(user.getEmail());
+        userResponseDTO.setPhoneNumber(user.getPhoneNumber());
+        userResponseDTO.setAddress(user.getAddress());
+        userResponseDTO.setRole(user.getRole());
+        userResponseDTO.setStatus(user.getStatus());
+        userResponseDTO.setCreatedDate(user.getCreatedDate());
+        userResponseDTO.setModifiedDate(user.getModifiedDate());
+
+        return userResponseDTO;
+    }
+
 }
